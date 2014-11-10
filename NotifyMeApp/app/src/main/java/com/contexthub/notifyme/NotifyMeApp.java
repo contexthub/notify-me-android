86cdd35a272c82ad87e36881a91c5fef711ca30d
Andrew Dyer <abdyer@gmail.com>
2014-11-10 17:35:55 -0600

    Update Gradle plugin/build tools/SDK versions. Simplify device push registration.

--- NotifyMeApp/app/src/main/java/com/contexthub/notifyme/NotifyMeApp.java
+++ NotifyMeApp/app/src/main/java/com/contexthub/notifyme/NotifyMeApp.java
@@ -2,17 +2,19 @@ package com.contexthub.notifyme;
 
 import android.app.Application;
 import android.content.Context;
+import android.os.Build;
+import android.text.TextUtils;
 import android.util.Log;
 
 import com.chaione.contexthub.sdk.ContextHub;
-import com.chaione.contexthub.sdk.DeviceProxy;
 import com.chaione.contexthub.sdk.SensorPipelineEvent;
 import com.chaione.contexthub.sdk.SensorPipelineListener;
-import com.chaione.contexthub.sdk.callbacks.Callback;
-import com.chaione.contexthub.sdk.model.Device;
 import com.chaione.contexthub.sdk.push.Push;
 import com.contexthub.notifyme.push.NotificationHandler;
 import com.pixplicity.easyprefs.library.Prefs;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by andy on 10/27/14.
@@ -34,37 +36,12 @@ public class NotifyMeApp extends Application implements SensorPipelineListener {
         super.onCreate();
         ContextHub.init(this, "YOUR-APP-ID-HERE");
         ContextHub.getInstance().addSensorPipelineListener(this);
-        Push.init(instance, "YOUR-GCM-PROJECT-ID-HERE", MainActivity.class, new NotificationHandler());
+
+        List<String> tags = Arrays.asList(getString(R.string.device_tag_1), getString(R.string.device_tag_2));
+        Push.init(instance, "YOUR-GCM-PROJECT-ID-HERE", Build.MODEL, tags, MainActivity.class, new NotificationHandler());
+
         Prefs.initPrefs(this);
-        setDeviceTags();
     }
-
-    private void setDeviceTags() {
-        final DeviceProxy proxy = new DeviceProxy();
-        proxy.getCurrentDevice(new Callback<Device>() {
-            @Override
-            public void onSuccess(Device device) {
-                device.getTags().add(getString(R.string.device_tag_1));
-                device.getTags().add(getString(R.string.device_tag_2));
-                proxy.updateDevice(device.getDeviceId(), device, updateDeviceCallback);
-            }
-
-            @Override
-            public void onFailure(Exception e) {
-                Log.d(getClass().getName(), e.getMessage());
-            }
-        });
-    }
-    private Callback<Device> updateDeviceCallback = new Callback<Device>() {
-        @Override
-        public void onSuccess(Device device) {
-        }
-
-        @Override
-        public void onFailure(Exception e) {
-            Log.d(getClass().getName(), e.getMessage());
-        }
-    };
 
     @Override
     public void onEventReceived(SensorPipelineEvent event) {
